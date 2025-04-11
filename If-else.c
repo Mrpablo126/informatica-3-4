fbcedbd3fe0dd7e8687a4a9af7061fa974606d37
Rodrigo <rlopezca89@gmail.com>
2025-04-11 14:49:47 +0000

    if 2

--- If-else.c
+++ If-else.c
@@ -5,7 +5,7 @@ int main () {
     // asjdalsjkf
     int num1;
     int num2;
-    
+    printf("1.\n");
     printf("Enter two numbers:");
     scanf("%d %d", &num1, &num2);
 
@@ -13,9 +13,21 @@ int main () {
     printf("%d is equal  to %d \n", num1, num2);
    } else{
     printf("%d is NOT equal  to %d \n", num1, num2);
+    
    }
-
-
+   // Even or Odd numbers
+   printf("2.\n");
+   int num;
+   printf("Enter a number:");
+   scanf("%d", &num);
+   if(num %2 == 0){
+    printf("%d is a even number \n", num);
+    } else {
+    printf("%d is a odd number \n", num);
+    
+    }
+   // ajslkfjasjfljsa
+    
 
 
 
--- variables-operations.c
+++ variables-operations.c
@@ -10,16 +10,20 @@ int main() {
     int Y = (width * 2 + length *2);
 
     printf("1. \n");
-    printf("Area = %d \n", X);
+
+    printf("enter rectangle width:");
+
+    scanf("%d", &width);
+    printf("enter rectangle lengh:");
+    scanf("%d", &length);
+        printf("Area = %d \n", X);
     printf("Perimeter = %d \n", Y);
+   
     printf("2. \n");
-    printf("enter rectangle lengh");
-    scanf("%d",&length);
     
-    //2
 
     int Fahrenheit;
-    printf("Enter temperature in Fahrenheit: ");
+    printf("Enter temperature in Fahrenheit:");
     scanf("%d", &Fahrenheit);
 
     int C = (Fahrenheit-32)/1.8;
@@ -29,13 +33,20 @@ int main() {
     //3
 
     int minutes;
-    printf("%d minutes", minutes);
-    scanf( %d, minutes);
+    printf("enter minutes:");
+    scanf("%d", &minutes);
     int hour = (minutes / 60);
     int extramins = (minutes %60);
     
     printf("%d hours and %d minutes \n", hour, extramins);
 
+    //
+    int radius;
+    printf("Enter a radius:");
+    scanf("%d", &radius);
+    int diameter = radius*2;
+    float perimeter = diameter*3.14;
+    printf("The perimeter of the circle is %.2f\n", perimeter);
 
     return 0;
 }
